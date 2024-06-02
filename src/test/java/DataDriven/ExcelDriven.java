074f2bc5f7037e0f05126c49f9e8a835258750d3
Dibyansh1998 <dibyansh.verma25@gmail.com>
2024-06-02 18:08:08 +0530

    Run the data from the Excel

--- src/test/java/DataDriven/ExcelDriven.java
+++ src/test/java/DataDriven/ExcelDriven.java
@@ -2,71 +2,71 @@ package DataDriven;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelDriven {
 
-	
-	
-	public static void main(String[] args) throws IOException {
+    public ArrayList<String> getData(String testcaseName) {
+        ArrayList<String> testData = new ArrayList<>();
 
-		// If you want to use excel data then create a object of XSSFWorkbook class and
-		// pass the excel path as
-		// FileInputStream object
+        String path = System.getProperty("user.dir") + "\\ExcelFolder\\ExcelDriven.xlsx";
+        try (FileInputStream fis = new FileInputStream(path);
+             XSSFWorkbook workbook = new XSSFWorkbook(fis)) {
 
-		FileInputStream fis = new FileInputStream(
-				"C:\\Users\\dibya\\OneDrive\\Desktop\\Desktop Items\\ExcelDriven.xlsx");
+            Sheet sheet = workbook.getSheet("TestData");
+            Iterator<Row> rows = sheet.iterator();
 
-		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+            int columnIndex = -1;
 
-		// Go to your desire sheet
-		int sheetCount = workbook.getNumberOfSheets();
-		for (int i = 0; i < sheetCount; i++) {
-			if (workbook.getSheetName(i).equalsIgnoreCase("TestData")) {
-				XSSFSheet sheet = workbook.getSheetAt(i);
+            // Find the column index for "TestCases"
+            Row firstRow = rows.next();
+            Iterator<Cell> cellIterator = firstRow.cellIterator();
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                if (cell.getStringCellValue().equalsIgnoreCase("TestCases")) {
+                    columnIndex = cell.getColumnIndex();
+                    break;
+                }
+            }
 
-				// Step 1: Identify the testCase column by scanning the entire 1st row
-				// This below command will move to iterate each of the row into the excel
-				Iterator<Row> rows = sheet.iterator();
-				Row firstrow = rows.next(); // This command will go to first row
+            if (columnIndex == -1) {
+                throw new RuntimeException("TestCases column not found in the Excel sheet.");
+            }
 
-				Iterator<Cell> ce = firstrow.cellIterator(); // Access to the first Cell
+            // Iterate over rows to find the matching testcaseName
+            while (rows.hasNext()) {
+                Row row = rows.next();
+                if (row.getCell(columnIndex).getStringCellValue().equalsIgnoreCase(testcaseName)) {
+                    // Fetch data from the matching row
+                    Iterator<Cell> cellIteratorForRow = row.cellIterator();
+                    while (cellIteratorForRow.hasNext()) {
+                        Cell cell = cellIteratorForRow.next();
+                        if (cell.getCellType() == CellType.STRING) {
+                            testData.add(cell.getStringCellValue());
+                        } else if (cell.getCellType() == CellType.NUMERIC) {
+                            testData.add(String.valueOf(cell.getNumericCellValue()));
+                        }
+                    }
+                    break; // Exit loop after finding the matching row
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-				int k = 0;
-				int column = 0;
-				while (ce.hasNext()) {
-					Cell value = ce.next();
-					if (value.getStringCellValue().equalsIgnoreCase("TestCases")) {
-						column=k;
-					}
-					k++;
-				}
-				System.out.println(column);
-				//Step 2: Once the column is identified then scan entire test column to identify purchase test case row
-				
-				while (rows.hasNext()) 
-				{
-					Row r=rows.next();
-					if(r.getCell(column).getStringCellValue().equalsIgnoreCase("Purchase"))
-					{
-						// Step 3: after you grab purchase testcase row= pull all the data from that row and feed into test.
-						Iterator<Cell> cv=r.cellIterator();
-						
-						while(cv.hasNext())
-						{
-							System.out.println(cv.next().getStringCellValue());
-						}
-					}
-				}
-			}
-			
-		}
+        return testData;
+    }
 
-	}
-
+    public static void main(String[] args) {
+        ExcelDriven excelDriven = new ExcelDriven();
+        ArrayList<String> testData = excelDriven.getData("TestCase1");
+        System.out.println("Test Data: " + testData);
+    }
 }
